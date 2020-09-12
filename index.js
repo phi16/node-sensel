39cfbcd244670a147c5f6175c71f19093222a19b
phi16 <phi16.256@gmail.com>
2020-09-12 21:22:46 +0900

    Completed

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const sensel = require('bindings')('sensel');
 
-const h = sensel.Open();
-console.log(h);
-sensel.StartScanning(h);
-
-while(true) {
-  sensel.Frame(h, (f,i,n)=>{
-    console.log(`${sensel.NumContacts(f)} / ${n}`);
-    return true;
-  });
+class SenselFrame {
+  constructor(frame) {
+    this.frame = frame;
+    this.numContacts = sensel.NumContacts(frame);
+  }
+  contact(cb) {
+    sensel.Contact(this.frame, (c,i,n)=>{
+      return cb(c,i,n);
+    });
+  }
 }
+
+class SenselDevice {
+  constructor(handle) {
+    this.handle = handle;
+    this.sensorInfo = sensel.SensorInfo(handle);
+    this.numLEDs = sensel.NumLEDs(handle);
+  }
+  close() {
+    sensel.Close(this.handle);
+  }
+  startScanning() {
+    sensel.StartScanning(this.handle);
+  }
+  stopScanning() {
+    sensel.StopScanning(this.handle);
+  }
+  setContactsMask(mask) {
+    sensel.SetContactsMask(this.handle, mask);
+  }
+  frame(cb) {
+    sensel.Frame(this.handle, (f,i,n)=>{
+      return cb(new SenselFrame(f), i, n);
+    });
+  }
+  LED(i, v) {
+    sensel.LED(this.handle, i, v);
+  }
+  LEDs(a) {
+    sensel.LEDs(this.handle, a);
+  }
+}
+
+exports.open = _=>{
+  try {
+    const h = sensel.Open();
+    return new SenselDevice(h);
+  } catch(e) {
+    return null;
+  }
+};
+exports.ContactsMask = sensel.ContactsMask;
+exports.ContactState = sensel.ContactState;
--- sensel-lib.cc
+++ sensel-lib.cc
@@ -18,37 +18,54 @@
   };
 #define InitWrapper(clName) SL##clName::Init(env, exports)
 
-DeclareWrapper(Handle, SENSEL_HANDLE raw; SenselFrameData* frame; unsigned short brightness);
+DeclareWrapper(Handle,
+  SENSEL_HANDLE raw;
+  SenselFrameData* frame;
+  unsigned char leds;
+  unsigned short brightness);
 DeclareWrapper(Frame, SenselFrameData* raw);
 
 struct Check {
   Napi::Env& env;
   Check(Napi::Env& env): env(env) {}
-  int operator+(SenselStatus status) {
+  void operator+(SenselStatus status) {
     if(status != SENSEL_OK) {
-      throw Napi::Error::New(this->env, "Sensel Error");
+      Napi::Error::New(this->env, "Sensel Error").ThrowAsJavaScriptException();
     }
-    return 0;
   }
 };
+struct CheckExit {
+  Napi::Env& env;
+  bool& exited;
+  CheckExit(Napi::Env& env, bool& exited): env(env), exited(exited) {}
+  void operator+(SenselStatus status) {
+    if(status != SENSEL_OK) {
+      Napi::Error::New(this->env, "Sensel Error").ThrowAsJavaScriptException();
+      exited = true;
+    }
+  }
+};
+#define CALLEXIT CheckExit(env, exited)+
 #define CALL Check(env)+
+#define THROW(...) if(exited) return __VA_ARGS__;
 
 Napi::Object slOpen(const Napi::CallbackInfo& info) {
+  bool exited = false;
   Napi::Env env = info.Env();
   SENSEL_HANDLE handle;
   SenselFrameData *frame = NULL;
+  unsigned char leds = 0;
   unsigned short brightness = 0;
-  CALL senselOpen(&handle);
+  CALLEXIT senselOpen(&handle); THROW(Napi::Object::New(env));
   CALL senselSetFrameContent(handle, FRAME_CONTENT_CONTACTS_MASK);
+  CALL senselGetNumAvailableLEDs(handle, &leds);
   CALL senselGetMaxLEDBrightness(handle, &brightness);
-  if(brightness != 100) {
-    CALL senselSetLEDBrightness(handle, 0, brightness);
-  }
 	CALL senselAllocateFrameData(handle, &frame);
   auto o = SLHandle::New(env);
   auto h = Napi::ObjectWrap<SLHandle>::Unwrap(o);
   h->raw = handle;
   h->frame = frame;
+  h->leds = leds;
   h->brightness = brightness;
   return o;
 }
@@ -71,22 +88,46 @@ void slStopScanning(const Napi::CallbackInfo& info) {
   CALL senselStopScanning(h->raw);
 }
 
+Napi::Number slNumLEDs(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  auto h = Napi::ObjectWrap<SLHandle>::Unwrap(info[0].As<Napi::Object>());
+  return Napi::Number::New(env, h->leds);
+}
+
 void slLED(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
   auto h = Napi::ObjectWrap<SLHandle>::Unwrap(info[0].As<Napi::Object>());
   unsigned int i = info[1].As<Napi::Number>();
   float v = info[2].As<Napi::Number>();
-  int b = v * h->brightness;
+  int b = v * v * h->brightness;
   if(b < 0) b = 0;
   if(b > h->brightness) b = h->brightness;
   CALL senselSetLEDBrightness(h->raw, i, (unsigned short) b);
 }
 
+void slLEDs(const Napi::CallbackInfo& info) {
+  bool exited = false;
+  Napi::Env env = info.Env();
+  auto h = Napi::ObjectWrap<SLHandle>::Unwrap(info[0].As<Napi::Object>());
+  auto ab = info[1].As<Napi::TypedArray>().ArrayBuffer();
+  float* d = reinterpret_cast<float*>(ab.Data());
+  int l = ab.ByteLength() / sizeof(float);
+  if(h->leds < l) l = h->leds;
+  for(int i = 0; i < l; i++) {
+    float v = d[i];
+    int b = v * v * h->brightness;
+    if(b < 0) b = 0;
+    if(b > h->brightness) b = h->brightness;
+    CALLEXIT senselSetLEDBrightness(h->raw, i, (unsigned short) b); THROW();
+  }
+}
+
 void slFrame(const Napi::CallbackInfo& info) {
+  bool exited = false;
   Napi::Env env = info.Env();
   auto h = Napi::ObjectWrap<SLHandle>::Unwrap(info[0].As<Napi::Object>());
   unsigned int frames = 0;
-  CALL senselReadSensor(h->raw);
+  CALLEXIT senselReadSensor(h->raw); THROW();
   CALL senselGetNumAvailableFrames(h->raw, &frames);
   Napi::Function cb = info[1].As<Napi::Function>();
   Napi::Number numFrames = Napi::Number::New(env, frames);
@@ -100,16 +141,89 @@ void slFrame(const Napi::CallbackInfo& info) {
       Napi::Number::New(env, i),
       numFrames
     });
-    if(v.As<Napi::Boolean>()) {
+    if(v.IsBoolean() && v.As<Napi::Boolean>()) {
       break;
     }
   }
+}
+
+#define Prop(n, v) o.Set(Napi::String::New(env, #n), Napi::Number::New(env, v))
+
+Napi::Object slSensorInfo(const Napi::CallbackInfo& info) {
+  bool exited = false;
+  Napi::Env env = info.Env();
+  auto h = Napi::ObjectWrap<SLHandle>::Unwrap(info[0].As<Napi::Object>());
+  SenselSensorInfo i;
+  CALLEXIT senselGetSensorInfo(h->raw, &i); THROW(Napi::Object::New(env));
+  auto o = Napi::Object::New(env);
+  Prop(max_contacts, i.max_contacts);
+  Prop(rows, i.num_rows);
+  Prop(cols, i.num_cols);
+  Prop(width, i.width);
+  Prop(height, i.height);
+  return o;
 }
 
 Napi::Number slNumContacts(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
   auto f = Napi::ObjectWrap<SLFrame>::Unwrap(info[0].As<Napi::Object>());
   return Napi::Number::New(env, f->raw->n_contacts);
+}
+
+void slContact(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  auto f = Napi::ObjectWrap<SLFrame>::Unwrap(info[0].As<Napi::Object>());
+  Napi::Function cb = info[1].As<Napi::Function>();
+  unsigned int n = f->raw->n_contacts;
+  Napi::Number numContacts = Napi::Number::New(env, n);
+  for(unsigned int i = 0; i < n; i++) {
+    SenselContact &c = f->raw->contacts[i];
+    Napi::Object o = Napi::Object::New(env);
+    Prop(id, c.id);
+    Prop(state, c.state);
+    Prop(x, c.x_pos);
+    Prop(y, c.y_pos);
+    Prop(force, c.total_force);
+    Prop(area, c.area);
+    if(c.content_bit_mask & CONTACT_MASK_ELLIPSE) {
+      Prop(orientation, c.orientation);
+      Prop(major_axis, c.major_axis);
+      Prop(minor_axis, c.minor_axis);
+    }
+    if(c.content_bit_mask & CONTACT_MASK_DELTAS) {
+      Prop(delta_x, c.delta_x);
+      Prop(delta_y, c.delta_y);
+      Prop(delta_force, c.delta_force);
+      Prop(delta_area, c.delta_area);
+    }
+    if(c.content_bit_mask & CONTACT_MASK_BOUNDING_BOX) {
+      Prop(min_x, c.min_x);
+      Prop(min_y, c.min_y);
+      Prop(max_x, c.max_x);
+      Prop(max_y, c.max_y);
+    }
+    if(c.content_bit_mask & CONTACT_MASK_PEAK) {
+      Prop(peak_x, c.peak_x);
+      Prop(peak_y, c.peak_y);
+      Prop(peak_force, c.peak_force);
+    }
+
+    Napi::Value v = cb.Call(env.Global(), {
+      o,
+      Napi::Number::New(env, i),
+      numContacts
+    });
+    if(v.IsBoolean() && v.As<Napi::Boolean>()) {
+      break;
+    }
+  }
+}
+
+void slSetContactsMask(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  auto h = Napi::ObjectWrap<SLHandle>::Unwrap(info[0].As<Napi::Object>());
+  unsigned int mask = info[1].As<Napi::Number>();
+  CALL senselSetContactsMask(h->raw, mask);
 }
 
 #define Declare(name) exports.Set(Napi::String::New(env, #name), Napi::Function::New(env, sl##name))
@@ -120,9 +234,28 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
   Declare(Close);
   Declare(StartScanning);
   Declare(StopScanning);
+  Declare(NumLEDs);
   Declare(LED);
+  Declare(LEDs);
   Declare(Frame);
+  Declare(SensorInfo);
   Declare(NumContacts);
+  Declare(Contact);
+  Declare(SetContactsMask);
+  auto contactsMask = Napi::Object::New(env);
+  contactsMask.Set(Napi::String::New(env, "NONE"), Napi::Number::New(env, 0));
+  contactsMask.Set(Napi::String::New(env, "ELLIPSE"), Napi::Number::New(env, CONTACT_MASK_ELLIPSE));
+  contactsMask.Set(Napi::String::New(env, "DELTAS"), Napi::Number::New(env, CONTACT_MASK_DELTAS));
+  contactsMask.Set(Napi::String::New(env, "BOUNDING_BOX"), Napi::Number::New(env, CONTACT_MASK_BOUNDING_BOX));
+  contactsMask.Set(Napi::String::New(env, "PEAK"), Napi::Number::New(env, CONTACT_MASK_PEAK));
+  contactsMask.Set(Napi::String::New(env, "ALL"), Napi::Number::New(env, 0xf));
+  exports.Set(Napi::String::New(env, "ContactsMask"), contactsMask);
+  auto contactState = Napi::Object::New(env);
+  contactState.Set(Napi::String::New(env, "INVALID"), Napi::Number::New(env, CONTACT_INVALID));
+  contactState.Set(Napi::String::New(env, "START"), Napi::Number::New(env, CONTACT_START));
+  contactState.Set(Napi::String::New(env, "MOVE"), Napi::Number::New(env, CONTACT_MOVE));
+  contactState.Set(Napi::String::New(env, "END"), Napi::Number::New(env, CONTACT_END));
+  exports.Set(Napi::String::New(env, "ContactState"), contactState);
   return exports;
 }
 
